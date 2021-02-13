695170e1997765de5a30c109a28927a5120b064b
zhukv98 <zhukevin98@gmail.com>
2021-02-13 15:23:55 -0500

    Fixed the Weather Tile issue

--- app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivity.kt
+++ app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivity.kt
@@ -57,43 +57,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-  //      setUpMapIfNeeded()
-    }
-
-//    override fun onResume() {
-//        super.onResume()
-//        setUpMapIfNeeded()
-//    }
-
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call [.setUpMap] once when [.mMap] is not null.
-     *
-     *
-     * If it isn't installed [SupportMapFragment] (and
-     * [MapView][com.google.android.gms.maps.MapView]) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     *
-     *
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), [.onCreate] may not be called again so we should call this
-     * method in [.onResume] to guarantee that it will be called.
-     */
-    private fun setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            val mapFragment = supportFragmentManager
-                    .findFragmentById(R.id.map) as SupportMapFragment
-            mapFragment.getMapAsync(this)
-        }
-        // Check if we were successful in obtaining the map.
-        else {
-            setUpMap()
-        }
     }
 
     /**
