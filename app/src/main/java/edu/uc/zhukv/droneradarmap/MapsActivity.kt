cf661b409034bf854d9cf28e04ac3df58359f5a3
zhukv98 <zhukevin98@gmail.com>
2021-03-04 17:46:19 -0500

    Added Device Location

--- app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivity.kt
+++ app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivity.kt
@@ -1,7 +1,15 @@
 package edu.uc.zhukv.droneradarmap
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,13 +22,94 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private val FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
+    private val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+    private var mLocationPermissionGranted = false
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1234
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private val DEFAULT_ZOOM = 15F
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        getLocationPermission()
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    private fun getDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (mLocationPermissionGranted) {
+            if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            val location = mFusedLocationProviderClient.lastLocation
+            location.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    var currentLocation = Location(task.result)
+                    moveCamera(
+                            LatLng(currentLocation.latitude, currentLocation.longitude),
+                            DEFAULT_ZOOM
+                    )
+                }
+            }
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng, zoom: Float) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+    }
+
+    private fun getLocationPermission() {
+        var permissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (ContextCompat.checkSelfPermission(
+                        this.applicationContext,
+                        FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ContextCompat.checkSelfPermission(
+                            this.applicationContext,
+                            COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                mLocationPermissionGranted = true
+            } else {
+                ActivityCompat.requestPermissions(
+                        this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE)
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, @NonNull permissions: Array<String>, @NonNull grantResults: IntArray)
+    {
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty()) {
+                    for(permission in grantResults){
+                        if(permission != PackageManager.PERMISSION_GRANTED){
+                            return
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -36,8 +125,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (mLocationPermissionGranted) {
+            getDeviceLocation()
+            if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            mMap.isMyLocationEnabled = true;
+        }
     }
 }
