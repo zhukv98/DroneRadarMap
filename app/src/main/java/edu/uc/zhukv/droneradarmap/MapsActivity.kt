4fd39a312a7253d53b4dadd47e81309d58b1fdae
zhukv98 <zhukevin98@gmail.com>
2021-03-05 21:58:20 -0500

    Text Box Added

--- app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivity.kt
+++ app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivity.kt
@@ -1,7 +1,14 @@
 package edu.uc.zhukv.droneradarmap
 
+import android.location.Address
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,16 +20,41 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    private lateinit var mSearchText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        mSearchText = findViewById(R.id.input_search)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-
+    private fun init(){
+        mSearchText.setOnEditorActionListener{ textView: TextView, actionId: Int, keyEvent: KeyEvent ->
+            override fun onEditorAction(textView: TextView, actionId: Int, keyEvent: KeyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction()==KeyEvent.ACTION_DOWN
+                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+                    geoLocate()
+                    true
+                }
+                false
+            }
+        }
+    }
+    private fun geoLocate(){
+        val searchString = mSearchText.text.toString()
+        val geocoder = Geocoder(this@MapsActivity)
+        val list: List<Address>
+        list = geocoder.getFromLocationName(searchString, 1)
+        if(list.isNotEmpty()){
+            val address = list[0];
+            Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -39,5 +71,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        init()
     }
 }
