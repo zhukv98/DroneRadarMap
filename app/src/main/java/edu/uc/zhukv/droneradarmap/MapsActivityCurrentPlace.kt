5d5ea56c5a89df8134628532a370bf94b58f2052
Storm Hamilton <hamilsu@mail.uc.edu>
2021-04-01 19:54:58 -0400

    Replaced Merged MapsActivityCurrentPlace.kt with the LocationLookup branch's version of the file. Should help alleviate all of the problems with the file caused by the merge, although there may be a bit of code loss.

--- app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivityCurrentPlace.kt
+++ app/src/main/java/edu/uc/zhukv/droneradarmap/MapsActivityCurrentPlace.kt
@@ -10,22 +10,15 @@ import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
-//Put it at the top of the file and converted to constants as that is their purpose. Also made public so that they can be accessed.
-public const val APP_ID = "c2b2b2a424915a775cacac7d33a2217a"
-public const val WEATHER_URL = "https://home.openweathermap.org/data/2.5/weather"
-
-public const val MIN_TIME: Long = 5000
-public const val MIN_DISTANCE = 1000f
-public const val REQUEST_CODE = 101
-
-
 class MapsActivityCurrentPlace(var NameOfCity: TextView? = null) {
+
+    val APP_ID = "c2b2b2a424915a775cacac7d33a2217a"
+    val WEATHER_URL = "https://home.openweathermap.org/data/2.5/weather"
 
     val MIN_TIME: Long = 5000
     val MIN_DISTANCE = 1000f
     val REQUEST_CODE = 101
     private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: Int = 0
-
     var locationProvider = LocationManager.GPS_PROVIDER
 
     var weatherIcon: ImageView? = null
@@ -34,7 +27,6 @@ class MapsActivityCurrentPlace(var NameOfCity: TextView? = null) {
 
     var mLocationManager: LocationManager? = null
     var mLocationListener: LocationManager? = null
-    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: Int = 0
 
     private val applicationContext: Context
         get() {
@@ -46,38 +38,15 @@ class MapsActivityCurrentPlace(var NameOfCity: TextView? = null) {
          * device. The result of the permission request is handled by a callback,
          * onRequestPermissionsResult.
          */
-
-        if (ContextCompat.checkSelfPermission(
-                this.applicationContext,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            == PackageManager.PERMISSION_GRANTED
-        ) {
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
             var locationPermissionGranted = true
         } else {
-            //Renamed because of underscores
-            val requestAccess = 0
+            val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0
             //ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
             //PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
         }
-
-
-    fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        var locationPermissionGranted = false
-        val requestAccess = null
-            if (ContextCompat.checkSelfPermission(this.applicationContext,
-                            Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                var locationPermissionGranted = true
-            } else {
-
-                //ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        //PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
-            }
 
 
     internal fun onRequestPermissionsResult(requestCode: Int,
@@ -86,12 +55,11 @@ class MapsActivityCurrentPlace(var NameOfCity: TextView? = null) {
         var locationPermissionGranted = false
 
         when (requestCode) {
-            requestAccess -> {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
 
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.isNotEmpty() &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     locationPermissionGranted = true
                 }
             }
