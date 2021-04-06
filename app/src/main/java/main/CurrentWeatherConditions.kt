2286a6c6ffab7636d743c7249c1559c3c4e83be6
Storm Hamilton <hamilsu@mail.uc.edu>
2021-04-06 18:42:43 -0400

    Reworked the logic in the CurrentWeatherConditions.kt (AKA the flag system)

--- app/src/main/java/main/CurrentWeatherConditions.kt
+++ app/src/main/java/main/CurrentWeatherConditions.kt
@@ -2,20 +2,59 @@ package main
 
 class CurrentWeatherConditions() {
 
-    val red_flag = 0
-    val yellow_flag = 1
-    val green_flag = 2
-    val temperature = 75
-    val windSpeed = 10
-    val precipitation = 0
+    var currentHazardRating = 0;
+
+    val GREEN_FLAG = 0
+    val YELLOW_FLAG = 1
+    val RED_FLAG = 2;
+    var temperature = 75
+    var windSpeed = 10
+    var precipitation = 0
 
     fun calculateCurrentFlag(): Int {
-        if (temperature == 75 && windSpeed == 10 && precipitation == 0 ) {
-            return green_flag
-        } else if(temperature < 75 && windSpeed < 10 && precipitation > 0) {
-            return yellow_flag
-        } else {
-            return red_flag
+        var currentFlag = 0;
+        calculateTemperatureHazard();
+        calculateWindHazard();
+        calculatePrecipitationHazard();
+
+        when(currentHazardRating) {
+            0 -> currentFlag = GREEN_FLAG
+            in 1..2 -> currentFlag = YELLOW_FLAG
+            else -> currentFlag = RED_FLAG
+        }
+        return currentFlag;
+    }
+
+    //Adds to the hazard rating depending on which range of numbers the current temp falls into.
+    //Basically a large piecewise function.
+    private fun calculateTemperatureHazard() {
+
+        when(temperature){
+            in 105..120 -> currentHazardRating+=2
+            in 95..105 -> currentHazardRating+=1
+            in 40..74 -> currentHazardRating+=1
+            in 0..39 -> currentHazardRating+=2
+            in -30..-1 -> currentHazardRating+=3
+        }
+    }
+
+    //Adds to the hazard rating depending on which range of numbers the current windspeed falls into
+    //Basically a large piecewise function.
+    private fun calculateWindHazard() {
+        when(windSpeed){
+            in 7..16 -> currentHazardRating +=1
+            in 17..25 -> currentHazardRating += 2
+            in 26..100 -> currentHazardRating += 3
+        }
+    }
+
+    //Adds to the hazard rating depending on which range of numbers the current chance of precipitation falls into
+    //Basically a large piecewise function.
+    private fun calculatePrecipitationHazard() {
+        when(precipitation){
+            in 10..30 -> currentHazardRating += 1
+            in 31..60 -> currentHazardRating += 2
+            in 61..101 -> currentHazardRating += 3
         }
     }
 }
