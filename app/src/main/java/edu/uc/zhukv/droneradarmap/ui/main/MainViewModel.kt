5206daeed2de2b2b2a51d480798f25c3a1bd9de8
zhukv98 <zhukevin98@gmail.com>
2021-04-11 18:55:34 -0400

    Aborted integration with DJI.

--- app/src/main/java/edu/uc/zhukv/droneradarmap/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/zhukv/droneradarmap/ui/main/MainViewModel.kt
@@ -6,7 +6,6 @@ import edu.uc.zhukv.droneradarmap.dto.Airport
 import edu.uc.zhukv.droneradarmap.service.AirportService
 
 class MainViewModel : ViewModel(){
-
     var airports: MutableLiveData<ArrayList<Airport>> = MutableLiveData()
     var airportService: AirportService = AirportService()
     fun fetchAirports(){
