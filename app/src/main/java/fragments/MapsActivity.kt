2442f8d115714e584d070457cc009f86a62ca29f
kootinja <kootinja@mail.uc.edu>
2021-04-21 13:36:48 -0400

    fixed maps ativity menu options

--- app/src/main/java/fragments/MapsActivity.kt
+++ app/src/main/java/fragments/MapsActivity.kt
@@ -1,7 +1,11 @@
 package fragments
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.uc.zhukv.droneradarmap.R
 
@@ -15,4 +19,21 @@ class MapsActivity : AppCompatActivity() {
         val adapter = ArrayAdapter.createFromResource(this, R.array.map_options, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
     }
+    //map options menu from the menu folder
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.map_options)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.normal_map -> Toast.makeText(this,"Normal Map", Toast.LENGTH_SHORT).show()
+            R.id.hybrid_map -> Toast.makeText(this, "Hybrid Map", Toast.LENGTH_SHORT).show()
+            R.id.satellite_map -> Toast.makeText(this, "Satellite Map", Toast.LENGTH_SHORT).show()
+            R.id.terrain_map-> Toast.makeText(this, "Terrain Map", Toast.LENGTH_SHORT).show()
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
